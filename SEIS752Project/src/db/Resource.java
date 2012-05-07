32db3d40b741f632e59ca38b98c1b4b32c098667
mike.seis752 <mike.seis752@6d2fb23a-976d-2c47-2b05-64ab85099205>
2012-05-07 04:38:10 +0000

    draft

--- SEIS752Project/src/db/Resource.java
+++ SEIS752Project/src/db/Resource.java
@@ -8,8 +8,12 @@ import java.sql.SQLException;
 import java.sql.Statement;
 
 public class Resource {
-	private static final String insertStatement = "insert into  Resources(ID,TITLE,DESCRIPTION,DATE_CREATED,URL,ICON,GRADE, SUBJECT,RESOURCE_TYPE) values (?,?,?,?,?,?,?,?,?)";
-	private static final String insertContentStatement = "insert into  Content(ID,FILE_TYPE,FILE_NAME,RESOURCES_ID) values (?,?,?,?)";
+	private static final String insertStatement = "insert into  Ressource(ID,TITLE,DESCRIPTION,DATE_CREATED,LOCATION,ICON) values (?,?,?,?,?,?)";
+	private static final String insertGradeStatement = "insert into  applies(RESSOURCE_ID,GRADE_ID) values (?,?)";
+	private static final String insertSubjectStatement = "insert into  relates(RESSOURCE_ID,SUBJECT_ID) values (?,?)";
+	String location = null;
+	String subject = null;
+	String grade = null;
 	String title = null;
 	String description = null;
 	Date date_created = null;
@@ -20,12 +24,12 @@ public class Resource {
 	private static final int TITLE = 2;
 	private static final int DESCRIPTION = 3;
 	private static final int DATE_CREATED = 4;
-	private static final int URL = 5;
+	private static final int LOCATION = 5;
 	private static final int ICON = 6;
-	private static final int GRADE = 7;
-	private static final int SUBJECT = 8;
-	private static final int RESOURCE_TYPE = 9;
-	
+	private static final int GRADE = 2;
+	private static final int SUBJECT = 2;
+
+
 	public Date getDate_created() {
 		return date_created;
 	}
@@ -51,16 +55,13 @@ public class Resource {
 	public void setResource_type(String resource_type) {
 		this.resource_type = resource_type;
 	}
-	public String getUrl() {
-		return url;
+	public String getLocation() {
+		return location;
 	}
-	public void setUrl(String url) {
-		this.url = url;
+	public void setLocation(String loc) {
+		this.location = loc;
 	}
-	String url = null;
-	String subject = null;
-	String grade = null;
-	String file = null;
+
 	public String getTitle() {
 		return title;
 	}
@@ -85,49 +86,61 @@ public class Resource {
 	public void setDescription(String description) {
 		this.description = description;
 	}
-	public String getFile() {
-		return file;
-	}
-	public void setFile(String file) {
-		this.file = file;
-	}
 	public int save(Connection conn) throws SQLException {
 		Statement stmt = conn.createStatement();
-		ResultSet idVal = stmt.executeQuery("select S_RESOURCE.nextVal from dual" );
+		ResultSet idVal = stmt.executeQuery("select S_RESSOURCE.nextVal from dual" );
 		if(idVal.next()) {
 			setId(idVal.getInt(1));
 		} else {
 			throw new SQLException("Error getting ID for Resources");
 		}
 		idVal.close();
-
+//  "insert into  Ressource(ID,TITLE,DESCRIPTION,DATE_CREATED,LOCATION,ICON) values (?,?,?,?,?,?,?,?,?)";
+/*
+  	private static final int ID = 1;
+	private static final int TITLE = 2;
+	private static final int DESCRIPTION = 3;
+	private static final int DATE_CREATED = 4;
+	private static final int LOCATION = 5;
+	private static final int ICON = 6;
+	private static final int GRADE = 7;
+	private static final int SUBJECT = 8;
+ */
 		PreparedStatement insertResourceStmt = conn.prepareStatement(insertStatement);
+		PreparedStatement insertGradeStmt = conn.prepareStatement(insertGradeStatement);
+		PreparedStatement insertSubjectStmt = conn.prepareStatement(insertSubjectStatement);
 		insertResourceStmt.setInt(ID, getId());
+		insertSubjectStmt.setInt(ID, getId());
+		insertGradeStmt.setInt(ID, getId());
 		insertResourceStmt.setDate(DATE_CREATED, getDate_created());
 		insertResourceStmt.setString(DESCRIPTION, getDescription());
-		insertResourceStmt.setString(GRADE, getGrade());
 		insertResourceStmt.setString(ICON, getIcon());
-		insertResourceStmt.setString(RESOURCE_TYPE, getResource_type());
-		insertResourceStmt.setString(SUBJECT, getSubject());
+		System.out.println("statement icon " + getIcon());
 		insertResourceStmt.setString(TITLE, getTitle());
-		insertResourceStmt.setString(URL, getUrl());
+		insertResourceStmt.setString(LOCATION, getLocation());
+		System.out.println("statement LOCATION " + getLocation());
+		insertGradeStmt.setString(GRADE, getGrade());
+		insertSubjectStmt.setString(SUBJECT, getSubject());
 		insertResourceStmt.executeUpdate();
+		insertGradeStmt.executeUpdate();
+		insertSubjectStmt.executeUpdate();
+
 		
-		PreparedStatement insertContentStmt = conn.prepareStatement(insertContentStatement);
-		ResultSet idContent = stmt.executeQuery("select S_CONTENT.nextVal from dual" );
-		if(idContent.next()){
-			insertContentStmt.setInt(1, idContent.getInt(1));
-			idContent.close();
-		} else {
-			idContent.close();
-			throw new SQLException("Error getting ID for Content");
-		}
+//		PreparedStatement insertContentStmt = conn.prepareStatement(insertContentStatement);
+//		ResultSet idContent = stmt.executeQuery("select S_CONTENT.nextVal from dual" );
+//		if(idContent.next()){
+//			insertContentStmt.setInt(1, idContent.getInt(1));
+//			idContent.close();
+//		} else {
+//			idContent.close();
+//			throw new SQLException("Error getting ID for Content");
+//		}
 		// ID,FILE_TYPE,FILE_NAME,RESOURCE_ID
-		insertContentStmt.setString(2,"");
-		insertContentStmt.setString(3, getFile());
-		insertContentStmt.setInt(4, id);
-		insertContentStmt.executeUpdate();
-		insertContentStmt.close();
+//		insertContentStmt.setString(2,"");
+//		insertContentStmt.setString(3, getFile());
+//		insertContentStmt.setInt(4, id);
+//		insertContentStmt.executeUpdate();
+//		insertContentStmt.close();
 		
 		return id;
 		
